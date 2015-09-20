c2460f97cb968ed5389b90895cb844473c78d090
Fabian <f.pena.jacobo@gmail.com>
2015-09-19 23:40:28 -0500

    Agregado acceso desde intermediary con RestClient

--- intermediary/app/controllers/welcome_controller.rb
+++ intermediary/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
+require 'open-uri'
 class WelcomeController < ApplicationController
 	def index
-		@products = JSON.parse(open("http://localhost:3000/products/index.json").read)
+		token = "86fca517b23dea388df2219948baa0ab"
+		@products = JSON.parse(RestClient.get("http://localhost:3000/products/index.json", {:Authorization => 'Token 86fca517b23dea388df2219948baa0ab' }))
 	end
 end
